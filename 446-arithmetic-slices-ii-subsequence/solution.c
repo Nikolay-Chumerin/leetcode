cffd814ed9665f9a26854454cd8083d2dbf6577a
Nikolay Chumerin (TME) <nikolay.chumerin@toyota-europe.com>
2024-01-09 11:00:08 +0100

    Add [446] Arithmetic Slices II - Subsequence brute-force solution

---
+++ 446-arithmetic-slices-ii-subsequence/solution.c
@@ -0,0 +1,129 @@
+/*
+ * @lc app=leetcode id=446 lang=c
+ *
+ * [446] Arithmetic Slices II - Subsequence
+ *
+ * https://leetcode.com/problems/arithmetic-slices-ii-subsequence/description/
+ *
+ * algorithms
+ * Hard (46.62%)
+ * Likes:    3218
+ * Dislikes: 148
+ * Total Accepted:    139.2K
+ * Total Submissions: 255.4K
+ * Testcase Example:  '[2,4,6,8,10]'
+ *
+ * Given an integer array nums, return the number of all the arithmetic
+ * subsequences of nums.
+ *
+ * A sequence of numbers is called arithmetic if it consists of at least three
+ * elements and if the difference between any two consecutive elements is the
+ * same.
+ *
+ *
+ * For example, [1, 3, 5, 7, 9], [7, 7, 7, 7], and [3, -1, -5, -9] are
+ * arithmetic sequences.
+ * For example, [1, 1, 2, 5, 7] is not an arithmetic sequence.
+ *
+ *
+ * A subsequence of an array is a sequence that can be formed by removing some
+ * elements (possibly none) of the array.
+ *
+ *
+ * For example, [2,5,10] is a subsequence of [1,2,1,2,4,1,5,10].
+ *
+ *
+ * The test cases are generated so that the answer fits in 32-bit integer.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [2,4,6,8,10]
+ * Output: 7
+ * Explanation: All arithmetic subsequence slices are:
+ * [2,4,6]
+ * [4,6,8]
+ * [6,8,10]
+ * [2,4,6,8]
+ * [4,6,8,10]
+ * [2,4,6,8,10]
+ * [2,6,10]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [7,7,7,7,7]
+ * Output: 16
+ * Explanation: Any subsequence of this array is arithmetic.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1  <= nums.length <= 1000
+ * -2^31 <= nums[i] <= 2^31 - 1
+ *
+ *
+ */
+
+// @lc code=start
+#include <stdbool.h>
+#define MAX_N (1000)
+
+/* Brute-force solution
+Just check all possible subsequences (containing more than 2 elements)
+of the original sequence and check them for being arithmetic.
+Works well for short input sequences, but get out of time already for the
+test#39:
+nums = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], len(nums) = 24
+expected answer 16776915.
+*/
+
+bool bits[MAX_N];
+int inc(const int n) {
+  bool carry = true;
+  int ans = 0;
+  for (int i = 0; i < n; ++i) {
+    const bool prev_val = bits[i];
+    bits[i] ^= carry;
+    carry = carry && prev_val;
+    ans += bits[i] ? 1 : 0;
+  }
+  return ans;
+}
+
+int numberOfArithmeticSlices(const int* nums, const int n) {
+  memset(bits, false, sizeof(bits));
+  int set_bit_num = 0;
+  int ans = 0;
+  int i = 0;
+  do {
+    set_bit_num = inc(n);
+    if (set_bit_num < 3) continue;
+    i = 0;
+    while ((i < n) && !bits[i]) {
+      ++i;
+    }
+
+    const long a0 = nums[i++];
+    while ((i < n) && !bits[i]) {
+      ++i;
+    };
+    long prev = nums[i++];
+    const long d = prev - a0;
+    do {
+      for (; (i < n) && !bits[i]; ++i)
+        ;
+      if (i >= n) break;
+      if ((long)nums[i] - (long)prev != d) break;
+      prev = nums[i];
+    } while (++i < n);
+    ans += (i == n);
+  } while (set_bit_num);
+  return ans;
+}
+
+// @lc code=end
